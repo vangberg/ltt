b7b87b4281990ffa1d3f1834598d9722bd5c458a
ichverstehe <ichverstehe@gmail.com>
2009-01-15 16:53:06 +0100

    js confirm delete entry

--- public/timetracker.js
+++ public/timetracker.js
@@ -2,4 +2,11 @@ $(function() {
   $(".project .bar").click(function() {
     $(this).parent().find(".entries").slideToggle(500);
   })
+
+  $(".entry button").click(function() {
+    if (confirm("Do you want to delete this entry?"))
+      return true;
+    else
+      return false;
+  })
 })
